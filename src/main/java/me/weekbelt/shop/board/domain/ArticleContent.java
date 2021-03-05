c42c8bf18d278e94580359ba2ab9cf14378d716b
weekbelt <vfrvfr4207@gmail.com>
2021-03-06 00:19:52 +0900

    feat: Create Article entity and ArticleContent value

---
+++ src/main/java/me/weekbelt/shop/board/domain/ArticleContent.java
@@ -0,0 +1,18 @@
+package me.weekbelt.shop.board.domain;
+
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class ArticleContent {
+
+    private String content;
+
+    private String contentType;
+}
